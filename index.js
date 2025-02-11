13fd73cdfc3ac387b588a847b698cc7efb1f57fd
Vikrant-1020 <165263680+Vikrant-1020@users.noreply.github.com>
2025-02-11 18:42:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello we are kuberenetes expert contact us');
+  res.send('Hello we are kuberenetes expert contact uskjhgfdg');
 });
 
 const port = process.env.PORT || 5000;
